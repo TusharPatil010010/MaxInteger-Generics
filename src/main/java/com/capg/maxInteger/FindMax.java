fcfa34a6caf0b08cbec286b1936f0df944d7f924
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-11 22:07:42 +0530

    Refactored the code using one generic method to find maximum value

--- src/main/java/com/capg/maxInteger/FindMax.java
+++ src/main/java/com/capg/maxInteger/FindMax.java
@@ -3,14 +3,15 @@ package com.capg.maxInteger;
 public class FindMax {
 
 	/**
-	 * UC1 Find the maximum from given inputs
+	 * UC4 find maximum from given inputs of any types using generics
 	 * 
+	 * @param <T>
 	 * @param first
 	 * @param second
 	 * @param third
 	 * @return
 	 */
-	public Integer findMaxInt(Integer first, Integer second, Integer third) {
+	public <T extends Comparable<T>> T findMaxValue(T first, T second, T third) {
 
 		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0)
 			return first;
@@ -20,46 +21,4 @@ public class FindMax {
 			return third;
 
 	}
-
-	/**
-	 * UC2 Find maximum from given float inputs
-	 * 
-	 * @param first
-	 * @param second
-	 * @param third
-	 * @return
-	 */
-	public Float findMaxFloat(Float first, Float second, Float third) {
-
-		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0)
-			return first;
-		else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0)
-			return second;
-		else
-			return third;
-
-	}
-
-	/**
-	 * UC3 Find maximumfrom given string input
-	 * 
-	 * @param first
-	 * @param second
-	 * @param third
-	 * @return
-	 */
-	public Integer findMaxString(String first, String second, String third) {
-
-		Integer a = Integer.parseInt(first);
-		Integer b = Integer.parseInt(second);
-		Integer c = Integer.parseInt(third);
-		if (a.compareTo(b) >= 0 && a.compareTo(b) >= 0)
-			return a;
-		else if (b.compareTo(a) >= 0 && b.compareTo(c) >= 0)
-			return b;
-		else
-			return c;
-
-	}
-
 }
--- src/test/java/com/capg/maxInteger/FindMaxTest.java
+++ src/test/java/com/capg/maxInteger/FindMaxTest.java
@@ -4,76 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FindMaxTest {
-
+	
 	@Test
-	public void givenThreeIntegerValue_WhenFirstIsMax_ShouldReturnFirstValue() {
+	public void givenThreeValues_ShouldReturnMaxValue() {
 		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxInt(8, 4, 2);
+		Integer maxNumber = findMaxObj.findMaxValue(8, 2, 3);
 		Integer expectedVal = 8;
 		Assertions.assertEquals(expectedVal, maxNumber);	
 	}
 	
-	@Test
-	public void givenThreeIntegerValue_WhenSecondIsMax_ShouldReturnSecondValue() {
-		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxInt(2, 8, 4);
-		Integer expectedVal = 8;
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeIntegerValue_WhenThirdIsMax_ShouldReturnThirdValue() {
-		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxInt(2, 4, 8);
-		Integer expectedVal = 8;
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeFloatValue_WhenFirstIsMax_ShouldReturnFirstValue() {
-		FindMax findMaxObj = new FindMax();
-		Float maxNumber = findMaxObj.findMaxFloat(8.0f, 4.0f, 2.0f);
-		Float expectedVal = 8.0f;
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeFloatValue_WhenSecondIsMax_ShouldReturnThirdValue() {
-		FindMax findMaxObj = new FindMax();
-		Float maxNumber = findMaxObj.findMaxFloat(2.0f, 8.0f, 4.0f);
-		Float expectedVal = 8.0f;
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeFloatValue_WhenThirdIsMax_ShouldReturnThirdValue() {
-		FindMax findMaxObj = new FindMax();
-		Float maxNumber = findMaxObj.findMaxFloat(2.0f, 4.0f, 8.0f);
-		Float expectedVal = 8.0f;
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeStringValue_WhenFirstIsMax_ShouldReturnFirstValue() {
-		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxString("8", "4", "2");
-		Integer expectedVal = Integer.parseInt("8");
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeStringValue_WhenSecondIsMax_ShouldReturnSecondValue() {
-		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxString("2", "8", "4");
-		Integer expectedVal = Integer.parseInt("8");
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
-	
-	@Test
-	public void givenThreeStringValue_WhenThirdIsMax_ShouldReturnThirdValue() {
-		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxString("2", "4", "8");
-		Integer expectedVal = Integer.parseInt("8");
-		Assertions.assertEquals(expectedVal, maxNumber);	
-	}
 }
