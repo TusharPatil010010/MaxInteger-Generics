f9d0bec2e6a5fbfb1578f271b3a311cefaa22255
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-11 22:38:20 +0530

    Refactored the code using one generic class to find maximum value

--- src/main/java/com/capg/maxInteger/FindMax.java
+++ src/main/java/com/capg/maxInteger/FindMax.java
@@ -1,21 +1,44 @@
 package com.capg.maxInteger;
 
-public class FindMax {
+public class FindMax<T extends Comparable<T>> {
+
+	T first;
+	T second;
+	T third;
 
 	/**
-	 * UC4 find maximum from given inputs of any types using generics
-	 * 
-	 * @param <T>
+	 * Parameterized constructor
+	 * @param first
+	 * @param second
+	 * @param third
+	 */
+	public FindMax(T first, T second, T third) {
+		super();
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
+	/**
+	 * UC3 Refactored #2, calls static testMaximum
+	 * @return
+	 */
+	public T testMaximum() {
+		return testMaximum(first, second, third);
+	}
+
+	/**
+	 * @param <E>
 	 * @param first
 	 * @param second
 	 * @param third
 	 * @return
 	 */
-	public <T extends Comparable<T>> T findMaxValue(T first, T second, T third) {
+	private static <E extends Comparable<E>> E testMaximum(E first, E second, E third) {
 
-		if (first.compareTo(second) >= 0 && first.compareTo(third) >= 0)
+		if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
 			return first;
-		else if (second.compareTo(first) >= 0 && second.compareTo(third) >= 0)
+		else if (second.compareTo(first) > 0 && second.compareTo(third) > 0)
 			return second;
 		else
 			return third;
--- src/test/java/com/capg/maxInteger/FindMaxTest.java
+++ src/test/java/com/capg/maxInteger/FindMaxTest.java
@@ -6,11 +6,20 @@ import org.junit.jupiter.api.Test;
 class FindMaxTest {
 	
 	@Test
-	public void givenThreeValues_ShouldReturnMaxValue() {
-		FindMax findMaxObj = new FindMax();
-		Integer maxNumber = findMaxObj.findMaxValue(8, 2, 3);
-		Integer expectedVal = 8;
-		Assertions.assertEquals(expectedVal, maxNumber);	
+	public void givenThreeIntegers_ShouldReturnMaxValue() {
+	    Integer expectedVal = 8;
+		Assertions.assertEquals(expectedVal, new FindMax<Integer>(8, 2, 4).testMaximum());	
 	}
 	
+	@Test
+	public void givenThreeFloats_ShouldReturnMaxValue() {
+	    Float expectedVal = 8.0f;
+		Assertions.assertEquals(expectedVal, new FindMax<Float>(8.0f, 2.0f, 4.0f).testMaximum());	
+	}
+	
+	@Test
+	public void givenThreeStrings_ShouldReturnMaxValue() {
+	    String expectedVal = "c";
+		Assertions.assertEquals(expectedVal, new FindMax<String>("a", "b", "c").testMaximum());	
+	}
 }
